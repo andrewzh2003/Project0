074e5bea9648b9d37473ab22edadd8844adc5e75
Alex Thornton <thornton@ics.uci.edu>
2022-09-07 14:15:57 -0700

    Initial commit
    Initial commit of Project 0 starting point.

---
+++ tests/test_queens.py
@@ -0,0 +1,27 @@
+# test_queens.py
+#
+# ICS 33 Fall 2022
+# Project 0: History of Modern
+#
+# Unit tests for the QueensState class in "queens.py".
+#
+# Docstrings are not required in your unit tests, though each test does need to have
+# a name that clearly indicates its purpose.  Notice, for example, that the provided
+# test method is named "test_zero_queen_count_initially" instead of something generic
+# like "test_queen_count", since it doesn't entirely test the "queen_count" method,
+# but instead focuses on just one aspect of how it behaves.  You'll want to do likewise.
+
+from queens import QueensState
+import unittest
+
+
+
+class TestQueensState(unittest.TestCase):
+    def test_zero_queen_count_initially(self):
+        state = QueensState(8, 8)
+        self.assertEqual(state.queen_count(), 0)
+
+
+
+if __name__ == '__main__':
+    unittest.main()
